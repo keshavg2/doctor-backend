2388bfd52d01ce2c1c850aec7549236ddb89c134
shivamscriptz7 <teamwork2177@gmail.com>
2026-05-20 18:09:30 +0530

    push new code of niginx fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { json, urlencoded } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
   });  
+
+  app.use(json({ limit: '100mb' }));
+  app.use(urlencoded({ extended: true, limit: '100mb' }));
+  
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
